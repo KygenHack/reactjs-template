7a6bd20922c5ab94f3bb0a2507d9e9ecf36561c3
KygenHack <148578932+KygenHack@users.noreply.github.com>
2024-06-29 13:39:15 -0400

    update

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -8,7 +8,7 @@ import tonSvg from './ton.svg';
 export const IndexPage: FC = () => {
   return (
     <List>
-      <Section
+      {/* <Section
         header='Features'
         footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
       >
@@ -20,21 +20,7 @@ export const IndexPage: FC = () => {
             TON Connect
           </Cell>
         </Link>
-      </Section>
-      <Section
-        header='Application Launch Data'
-        footer='These pages help developer to learn more about current launch information'
-      >
-        <Link to='/init-data'>
-          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
-        </Link>
-        <Link to='/launch-params'>
-          <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
-        </Link>
-        <Link to='/theme-params'>
-          <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
-        </Link>
-      </Section>
+      </Section> */}
     </List>
   );
 };
